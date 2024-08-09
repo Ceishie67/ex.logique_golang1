de59ce1f054f7eb7379d36862b3a9b8ef653a86e
Ceishie67 <champiloun.nathan@gmail.com>
2024-08-09 16:35:28 +0200

    debut du programme

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
+// Le but de cet exercice est de créer un motif grâce à des symboles comme * ou - par exemple
+
 package ex_logique_golang1
 
+import "fmt"
+
 func main() {
+	// main excute la fonction print_diamond
+	print_diamond()
+
+}
+
+func print_diamond() {
+	// Le motif a représenter dans cet exercice est un diamant
+	// le code doit afficher dans le terminal :
+	//
+	//	*****
+	//
+	// ********
+	//
+	//	*****
+	//	 ***
+	//	  *
+
+	fmt.Println("*")
 
 }
